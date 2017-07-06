8f2dc25cadd74264a5c238bf719aded23c375613
Richard A Zaslaw <richard.zaslaw@telerik.com>
2017-07-06 16:34:07 -0400

    Basic Kendo Grid for Angular
    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { products } from "../services/products";
+import { GridDataResult, PageChangeEvent } from '@progress/kendo-angular-grid';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  // load the default theme (use correct path to node_modules)
+  styleUrls: [
+      '../../node_modules/@progress/kendo-theme-default/dist/all.css'
+  ],
+  // prevent style encapsulation
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-  title = 'app';
+  private gridView: GridDataResult;
+    private data: Object[];
+    private pageSize: number = 10;
+    private skip: number = 0;
+    private items: any[] = products;
+
+    constructor() {
+        this.loadItems();
+    }
+
+    protected pageChange(event: PageChangeEvent): void {
+        this.skip = event.skip;
+        this.loadItems();
+    }
+
+    private loadItems(): void {
+        this.gridView = {
+            data: this.items.slice(this.skip, this.skip + this.pageSize),
+            total: this.items.length
+        };
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GridModule } from '@progress/kendo-angular-grid';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   bootstrap:    [AppComponent],
+   declarations: [AppComponent],
+   imports:      [BrowserModule, BrowserAnimationsModule, GridModule]
 })
-export class AppModule { }
+export class AppModule {
+}
